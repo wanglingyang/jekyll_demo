fa34456b0bd4afbc680b25040234e989395a6bca
695400840 <695400840@qq.com>
2016-11-01 21:32:15 +0800

    first post

--- js/number_slideshow.js
+++ js/number_slideshow.js
@@ -140,3 +140,27 @@
         }
     }
 })(jQuery);
+
+            $(function() {
+                $("#number_slideshow").number_slideshow({
+                    slideshow_autoplay: 'enable',//enable disable
+                    slideshow_time_interval: 3000,
+                    slideshow_window_background_color: "#fff",
+                    slideshow_window_padding: '0',
+                    slideshow_window_width: '1000',
+                    slideshow_window_height: '300',
+                    slideshow_border_size: '0',
+                    slideshow_transition_speed: 500, //'normal','slow','fast' or numeral
+                    slideshow_border_color: 'black',
+                    slideshow_show_button: 'enable',//enable disable
+                    slideshow_show_title: 'disable',//enable disable
+                    slideshow_button_text_color: '#CCC',
+                    slideshow_button_current_text_color: '#000',
+                    slideshow_button_background_color: 'red',
+                    slideshow_button_current_background_color: 'white',
+                    slideshow_button_border_color: '#000',
+                    slideshow_loading_gif: 'loading.gif',//loading pic, you can replace it.
+                    slideshow_button_border_size: '0'
+                }); 
+
+            });
---
+++ js/slideshow.js
@@ -0,0 +1,24 @@
+﻿var curIndex=0; 
+//时间间隔 单位毫秒 
+var timeInterval=4000; 
+var arr=new Array(); 
+arr[0]="css3.jpg"; 
+arr[1]="js.jpg"; 
+arr[2]="html5.jpg"; 
+arr[3]="page7.jpg"; 
+arr[4]="Photoshop.png";  
+
+function changeImg() 
+{ 
+var obj=document.getElementById("change"); 
+if (curIndex==arr.length-1) 
+	{ 
+		curIndex=0; 
+	} 
+else 
+	{ 
+		curIndex+=1; 
+	} 
+obj.src="images/"+arr[curIndex]; 
+} 
+setInterval(changeImg,timeInterval); 
