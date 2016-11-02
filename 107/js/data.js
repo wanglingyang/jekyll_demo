a8d9761585f2c0112a711969d3adefb05ba5adb9
695400840 <695400840@qq.com>
2016-11-03 00:49:40 +0800

    first post

---
+++ 107/js/data.js
@@ -0,0 +1,10 @@
+﻿function showTime(){
+var d=new Data();
+var year=d.getFullYear();
+var month=d.getMonth()+1;
+var day=d.getDate();
+document.getElementById("content2_3").innerHTML=(year+"/"+month+"/"+day);
+}
+
+window.onload=showTime();
+
